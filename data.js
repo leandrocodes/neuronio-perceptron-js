55abc6dc5002869e5f2237dd761aab97572d2148
leandrocodes <leaxviana140@gmail.com>
2019-10-15 20:11:04 -0400

    dataset and desired output

--- data.js
+++ data.js
@@ -1,3 +1,19 @@
-var table = [
-  
+const dataset = [
+  [1.0, 1.0],
+  [9.4, 6.4],
+  [2.5, 2.1],
+  [8.0, 7.7],
+  [0.5, 2.2],
+  [7.9, 8.4],
+  [7.0, 7.0],
+  [2.8, 0.8],
+  [1.2, 3.0],
+  [7.8, 6.1]
 ]
+
+const desiredOutput = [
+  1, -1, 1, -1, 1, -1, -1, 1, 1, 1 -1
+]
+
+console.table(dataset)
+console.log(desiredOutput)
