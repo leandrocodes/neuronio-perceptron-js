7d84bfb1b7196542398e48eb820de549a90460de
leandrocodes <leaxviana140@gmail.com>
2019-10-21 17:46:27 -0400

    prediction statement

--- neuron.js
+++ neuron.js
@@ -3,7 +3,10 @@ const Perceptron = size =>{
     weights: Array(++size).fill(0),
     predict (inputs) {
       let activation = this.weights[0]
-      console.log(activation)
+      for (input in inputs){
+        activation += this.weights[input+1] * inputs[input]
+      }
+      return activation >= 0 ? 1 : -1
     } 
   }
 }
