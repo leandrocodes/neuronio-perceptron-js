c53d51ccb24019fab765add8dc58a8afc453b0a7
leandrocodes <leaxviana140@gmail.com>
2019-11-12 15:21:29 -0400

    predicting but epochs number is too high

--- perceptron.js
+++ perceptron.js
@@ -2,7 +2,6 @@ module.exports = class Perceptron {
 
   constructor(x_train, y_train, epochs = 1000, learn_rate = 0.1) {
 
-    // used to generate percent accuracy
     this.accuracy = 0
     this.samples = 0
     this.x_train = x_train
@@ -14,63 +13,53 @@ module.exports = class Perceptron {
     this.bias = 0
     this.weights = new Array(x_train[0].length)
 
-    // initialize random weights
     for (let n = 0; n < x_train[0].length; n++) {
       this.weights[n] = this.random()
     }
   }
 
-  // returns percent accuracy 
+
   current_accuracy () {
     return this.accuracy / this.samples
   }
 
-  // generate random float between -1 and 1 (for generating weights)
   random () {
     return Math.random() * 2 - 1
   }
 
-  // activation function
+
   activation (n) {
     return n < 0 ? -1 : 1
   }
 
-  // y-hat output given an input tensor 
+
   predict (input) {
     let total = this.bias
-    this.weights.forEach((w, index) => { total += input[index] * w }) // multiply each weight by each input vector value
+    this.weights.forEach((w, index) => { total += input[index] * w })
     return this.activation(total)
   }
 
-  // training perceptron on data
   fit () {
-    // epochs loop
+
     for (let e = 0; e < this.epochs; e++) {
 
-      // for each training sample
       for (let i = 0; i < this.x_train.length; i++) {
 
-        // get prediction
         let prediction = this.predict(this.x_train[i])
         console.log('Saída desejada: ' + this.y_train[i] + '    Saída do Modelo: ' + prediction)
 
-        // update accuracy measures
         this.y_train[i] === prediction ? this.accuracy += 1 : this.accuracy -= 1
         this.samples++
 
-        // calculate loss
         let loss = this.y_train[i] - prediction
 
-        // update all weights
         for (let w = 0; w < this.weights.length; w++) {
           this.weights[w] += loss * this.x_train[i][w] * this.learn_rate
         }
 
-        // update bias
         this.bias += loss * this.learn_rate
       }
 
-      // accuracy post epoch
       console.log(this.current_accuracy())
       console.log(e)
     }
