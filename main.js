86036ae6591f80c99e456001ebeac42b0f26993b
leandrocodes <leaxviana140@gmail.com>
2019-11-12 14:18:10 -0400

    stucked at training

--- main.js
+++ main.js
@@ -1,14 +1,30 @@
-const percep = Perceptron(dataset.length)
+let perceptron = require('perceptron')
 
-// console.log(percep)
+let pcpt = perceptron()
 
-//percep.predict()
-percep.fit(dataset, desiredOutput)
+pcpt.weights = [0.75, 0.5, -0.6]
 
-//percep.predict([1.0, 1.0])
+pcpt.train([1, 1], 1)
+pcpt.train([9.4, 6.4], -1)
+pcpt.train([2.5, 2.1], 1)
+pcpt.train([8, 7.7], -1)
+pcpt.train([0.5, 2.2], 1)
+pcpt.train([7.9, 8.4], -1)
+pcpt.train([7, 7], -1)
+pcpt.train([2.8, 0.8], 1)
+pcpt.train([1.2, 3.0], 1)
+pcpt.train([7.8, 6.1], -1)
 
-let results = document.querySelector('#results')
 
-for(let i = 0; i<dataset.length; i++){
-  results.innerHTML += `<li>${percep.predict(i)}</li>`
+let epochs = 0
+while(!pcpt.retrain()) {
+  epochs++
 }
+console.log(epochs)
+
+pcpt.perceive([1, 1]) // => 1
+pcpt.perceive([0, 1]) // => 0
+pcpt.perceive([1, 0]) // => 0
+pcpt.perceive([0, 0]) // => 0
+console.log(pcpt.weights)
+console.log(pcpt.delta)
